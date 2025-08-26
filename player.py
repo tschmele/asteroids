9b85d67feeddbb153537855d8f837685a8a27402
tschmele <tristan.schmele@gmail.com>
2025-08-26 18:01:14 +0200

    limit firerate

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cd = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,6 +19,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cd -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -27,7 +29,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.cd <= 0:
+            self.cd = PLAYER_SHOOT_COOLDOWN
             self.shoot()
     
     def draw(self, screen):
